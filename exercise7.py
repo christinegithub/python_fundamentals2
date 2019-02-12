ccd36e16177effc2f8d47c7815449a834d328d22
christinegithub <christine13@gmail.com>
2019-02-12 14:26:23 -0500

    exercise7 complete

---
+++ exercise7.py
@@ -0,0 +1,30 @@
+# Let's create a function wrap_text that wraps text in symbols of our choice. For example:
+
+# wrap_text('hello', '===')
+# should return:
+
+# ===hello===
+
+# Now that this function works, how can we use it (without modifying the function)
+# to generate the following string?
+
+# ---===###new message###===---
+
+# Note that wrap_text needs to return a value rather than print one.
+
+# Hints:
+#
+# You'll have to call the same function multiple times.
+# Try breaking down the problem into smaller pieces that you know wrap_text can solve.
+
+word = ''
+wrap = ''
+
+def wrap_text(word, wrap):
+    return wrap + word + wrap
+
+print(wrap_text('new message', '###'))
+
+print(wrap_text(wrap_text('new message', '###'), '==='))
+
+print(wrap_text(wrap_text(wrap_text('new message', '###'), '==='), '---'))
