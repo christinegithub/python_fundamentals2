01b99e20e16d8ba874a9e2337e67c9ea2ffa6ddf
christinegithub <christine13@gmail.com>
2019-02-12 13:10:19 -0500

    exercise3 complete

---
+++ exercise3.py
@@ -0,0 +1,10 @@
+# Define a function called is_even that accepts a number as an argument and
+# returns a boolean (true/false) indicating whether that number is even or not
+# (HINT: use the % operator).
+# Try calling it with different numbers.
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
